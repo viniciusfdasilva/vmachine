9dfb7e3aa2ba38a32404a351535a8f74e3c580f9
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2020-06-19 14:21:15 -0300

    [vmachine] Feature: Initial Source Tree

---
+++ include/arch/mips32/registers.h
@@ -0,0 +1,67 @@
+/*
+ * MIT License
+ *
+ * Copyright(c) 2011-2020 The Maintainers of Nanvix
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef ARCH_MIPS32_REGISTERS_H_
+#define ARCH_MIPS32_REGISTERS_H_
+
+	/**
+	 * @name Code of Registers
+	 */
+	/**@{*/
+	#define REG_ZERO_NUM_STR  0x00
+	#define REG_AT_NUM_STR    0x01
+	#define REG_V0_NUM_STR    0x02
+	#define REG_V1_NUM_STR    0x03
+	#define REG_A0_NUM_STR    0x04
+	#define REG_A1_NUM_STR    0x05
+	#define REG_A2_NUM_STR    0x06
+	#define REG_A3_NUM_STR    0x07
+	#define REG_T0_NUM_STR    0x08
+	#define REG_T1_NUM_STR    0x09
+	#define REG_T2_NUM_STR    0x0a
+	#define REG_T3_NUM_STR    0x0b
+	#define REG_T4_NUM_STR    0x0c
+	#define REG_T5_NUM_STR    0x0d
+	#define REG_T6_NUM_STR    0x0e
+	#define REG_T7_NUM_STR    0x0f
+	#define REG_S0_NUM_STR    0x10
+	#define REG_S1_NUM_STR    0x11
+	#define REG_S2_NUM_STR    0x12
+	#define REG_S3_NUM_STR    0x13
+	#define REG_S4_NUM_STR    0x14
+	#define REG_S5_NUM_STR    0x15
+	#define REG_S6_NUM_STR    0x16
+	#define REG_S7_NUM_STR    0x17
+	#define REG_T8_NUM_STR    0x18
+	#define REG_T9_NUM_STR    0x19
+	#define REG_K0_NUM_STR    0x1a
+	#define REG_K1_NUM_STR    0x1b
+	#define REG_GP_NUM_STR    0x1c
+	#define REG_SP_NUM_STR    0x1d
+	#define REG_FP_NUM_STR    0x1e
+	#define REG_RA_NUM_STR    0x1f
+	/**@}**/
+
+#endif /* ARCH_MIPS32_REGISTERS_H_ */
+
