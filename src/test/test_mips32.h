c7cc41a0d549624e9cb496a841f0b70cc52d59c4
Leonardo Henrique Guimaraes <leonardo.guimaraes20@icloud.com>
2020-07-01 20:42:58 -0300

    [assembler] Feature: Initial test structure

---
+++ src/test/test_mips32.h
@@ -0,0 +1,38 @@
+/*
+ * MIT License
+ *
+ * Copyright(c) 2011-2020 The Maintainers of Nanvix
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef _ASSEMBLER_MIPS_32_TEST_H
+#define _ASSEMBLER_MIPS_32_TEST_H
+
+	struct test 
+	{
+		void (*test_fn)(void);
+		const char *name;
+	};
+
+	EXTERN const char *HLINE;
+
+	EXTERN void test_parse_Uint32(void);
+
+#endif
