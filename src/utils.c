12f41e32efb153905ffcc9399b20d797e3efa478
Vinicius Silva <vinicius.silva.1046664@sga.pucminas.br>
2020-08-24 19:00:19 -0300

    [utils] Feature: Add string and math libraries

--- src/utils.c
+++ src/utils.c
@@ -25,6 +25,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
+#include <math.h>
 
 /*============================================================================*
  * error()                                                                    *
