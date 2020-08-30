0c06c651a14ab9c26ed7a3f828d2c44c9278c046
Vinicius Silva <vinicius.silva.1046664@sga.pucminas.br>
2020-08-30 02:46:07 -0300

    [vmachine] Refactor: Values refactor of the struct r_inst

--- src/vmachine/engine.c
+++ src/vmachine/engine.c
@@ -22,8 +22,13 @@
  * SOFTWARE.
  */
 
+#include <vmachine.h>
+#include <asm/mips32.h>
+#include <utils.h>
 
 R_instruction* r_inst;
+
+#define SYNTATIC_ERROR "Syntatic Error"
 
 /**
  * Check if these two parameters are equals.
@@ -51,14 +56,22 @@ static void r_procedure_Format()
 
 }
 
+/**
+ * r_pocedure_functionOpcode(); 
+ */
+static void r_pocedure_functionOpcode()
+{
+
+}
+
 /*
  * First rule of grammar and Parsing tree
 */
 static void r_procedure_S()
 {
-	if(equals(R_OPCODE,r_inst->opcode))
+	if(equals(R_OPCODE,r_inst->instruction->opcode))
 	{
-		match(R_OPCODE,r_inst->opcode);
+		//match(R_OPCODE,r_inst->instruction->opcode);
 	}
 	r_procedure_Format();
 	r_pocedure_functionOpcode();
@@ -79,20 +92,18 @@ static void r_translator()
  * and redirects for each function corresponding
  * @param inst[] MIPS instruction of 32 bits
  * */
-void select(char inst[])
+void select(char instruction[])
 {
 		
-	char* opcode = substring(inst,0,5);
+	char* opcode = substring(instruction,0,5);
 
 	if(equals(opcode,R_OPCODE)){
-		r_inst = init();
-
-		r_inst->opcode = opcode;
-		r_inst->rs = substring(inst,6,10);
-		r_inst->rt = substring(inst,11,15);
-		r_inst->rd = substring(inst,16,20);
-		r_inst->shamt = substring(inst,21,25);
-		r_inst->funct = substring(inst,26,31);
+		r_inst->instruction->opcode = opcode;
+		r_inst->rs->code = substring(instruction,6,10);
+		r_inst->rt->code = substring(instruction,11,15);
+		r_inst->rd->code = substring(instruction,16,20);
+		//r_inst->instruction->encode = substring(instruction,21,25);
+		r_inst->instruction->funct = substring(instruction,26,31);
 		r_translator();
 	}
 }
@@ -103,7 +114,7 @@ void select(char inst[])
 word_t engine_run(uint32_t instruction)
 {
 	char string_instruction[32];
-	itoa(instruction,string_instruction,10);
+	itoa2(instruction,string_instruction,10);
 	select(string_instruction);
 
 	return (instruction);
