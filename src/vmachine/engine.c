d6883186e1aa18a144e75fe45b9a8a4cf853af04
Vinicius Silva <vinicius.silva.1046664@sga.pucminas.br>
2020-09-08 08:56:16 -0300

    [vmachine] Feature: Adding registers rules

--- src/vmachine/engine.c
+++ src/vmachine/engine.c
@@ -28,24 +28,86 @@
 
 R_instruction* r_inst;
 
-#define SYNTATIC_ERROR "Syntatic Error"
+#define SYNTATIC_ERROR "Syntatic Error!"
 
 #define opcode r_inst->instruction->opcode
-#define encode r_inst->instruction->encode
+#define shamt r_inst->shamt
 #define funct r_inst->instruction->funct
+
 #define rd r_inst->rd->code
 #define rs r_inst->rs->code
 #define rt r_inst->rt->code
+#define sa r_inst->sa->code
+
+#define REGISTERS 32
+
+const char* registers[REGISTERS] = {
+	REG_ZERO_NUM_STR,
+	REG_AT_NUM_STR, REG_V0_NUM_STR, REG_V1_NUM_STR, 
+	REG_A0_NUM_STR, REG_A1_NUM_STR, REG_A2_NUM_STR,
+	REG_A3_NUM_STR, REG_T0_NUM_STR, REG_T1_NUM_STR,
+	REG_T2_NUM_STR, REG_T3_NUM_STR, REG_T4_NUM_STR,
+	REG_T5_NUM_STR, REG_T6_NUM_STR, REG_T7_NUM_STR,
+	REG_S0_NUM_STR, REG_S1_NUM_STR, REG_S2_NUM_STR,
+	REG_S3_NUM_STR, REG_S4_NUM_STR, REG_S5_NUM_STR,
+	REG_S6_NUM_STR, REG_S7_NUM_STR, REG_T8_NUM_STR,
+	REG_T9_NUM_STR, REG_K0_NUM_STR, REG_K1_NUM_STR,
+	REG_GP_NUM_STR, REG_SP_NUM_STR, REG_FP_NUM_STR,
+	REG_RA_NUM_STR,
+};
 
 /**
  * Check if these two parameters are equals.
  * @param tk1
  * @param tk2
  * */
-static void match(char* tk1,char* tk2)
+static void match(const char* tk1,const char* tk2)
 {
+	if(!equals(tk1,tk2)) printf("%s Between: %s and %s codes",SYNTATIC_ERROR,tk1,tk2);
+}
 
-	if(!equals(tk1,tk2)) printf("%s",SYNTATIC_ERROR);
+/*
+* r_procedure_D();
+*/
+static void r_procedure_D()
+{
+	if(equals(shamt,"00000"))
+	{
+		match(shamt,"00000");
+	}else
+	{
+		match(sa,registers[atoi2((char*)sa)]);
+	}
+}
+
+/*
+* r_procedure_C();
+*/
+static void r_procedure_C()
+{
+	if(equals(shamt,"0000000000"))
+	{
+		match(shamt,"0000000000");
+	}else
+	{
+		match(rd,registers[atoi2((char*)rd)]);
+		r_procedure_D();
+	}
+}
+
+/*
+* r_procedure_B();
+*/
+static void r_procedure_B()
+{
+	if(equals(shamt,"000000000000000"))
+	{
+		match(shamt,"000000000000000");
+	}else
+	{
+		match(rt,registers[atoi2((char*)rt)]);
+		r_procedure_C();
+	}
 }
 
 /*
@@ -53,7 +115,8 @@ static void match(char* tk1,char* tk2)
 */
 static void r_procedure_Format()
 {
-
+	match(rs,registers[atoi2((char*)rs)]);
+	r_procedure_B();
 }
 
 /**
@@ -235,20 +298,51 @@ static void r_translator()
  * @param inst[] MIPS instruction of 32 bits
  * */
 void select(char instruction[])
-{
-		
+{	
 	char* op = substring(instruction,0,5);
 
 	if(equals(op,R_OPCODE))
 	{
 		r_inst = init();
-
 		opcode = op;
-		rs = substring(instruction,6,10);
-		rt = substring(instruction,11,15);
-		rd = substring(instruction,16,20);
-		encode = substring(instruction,21,25);
 		funct = substring(instruction,26,31);
+		
+		int func = atoi2((char*)funct);
+
+		if(func==INST_ADD_FUNCT || func==INST_ADDU_FUNCT || func==INST_SUB_FUNCT || func==INST_SUBU_FUNCT)
+		{
+			rs = substring(instruction,6,10);
+			rt = substring(instruction,11,15);
+			rd = substring(instruction,16,20);
+			shamt = substring(instruction,21,25);
+		}else if(func==INST_MULT_FUNCT || func==INST_MULTU_FUNCT || func==INST_DIV_FUNCT || func==INST_DIVU_FUNCT)
+		{
+			rs = substring(instruction,6,10);
+			rt = substring(instruction,11,15);
+			shamt = substring(instruction,16,25);
+		}else if(func==INST_JR_FUNCT)
+		{
+			rs = substring(instruction,6,10);
+			shamt = substring(instruction,11,25);
+		}else if(func==INST_AND_FUNCT || func==INST_NOR_FUNCT || func==INST_OR_FUNCT)
+		{
+			rs = substring(instruction,6,10);
+			rt = substring(instruction,11,15);
+			rd = substring(instruction,16,20);
+			shamt = substring(instruction,21,25);
+		}else if(func==INST_SLT_FUNCT || func==INST_SLTU_FUNCT)
+		{
+			rs = substring(instruction,6,10);
+			rt = substring(instruction,11,15);
+			rd = substring(instruction,16,20);
+			shamt = substring(instruction,21,25);
+		}else if(func==INST_SLL_FUNCT || func==INST_SRL_FUNCT || func==INST_SRA_FUNCT)
+		{
+			rs = substring(instruction,6,10);
+			rt = substring(instruction,11,15);
+			rd = substring(instruction,16,20);
+			sa = substring(instruction,21,25);
+		}
 		r_translator();
 	}
 }
